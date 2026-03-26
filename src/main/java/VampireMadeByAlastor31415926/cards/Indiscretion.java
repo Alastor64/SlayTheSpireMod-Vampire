5aa0fb7b385beea19499ef746ef3f88230abd16b
Alastor64 <915789546@qq.com>
2026-03-26 10:38:18 +0800

    it works

--- src/main/java/VampireMadeByAlastor31415926/cards/Indiscretion.java
+++ src/main/java/VampireMadeByAlastor31415926/cards/Indiscretion.java
@@ -8,26 +8,17 @@ import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.VulnerablePower;
 
-import static VampireMadeByAlastor31415926.characters.MyCharacter.PlayerColorEnum.COLOR_BLOOD;
+import VampireMadeByAlastor31415926.helpers.MyCard;
 import VampireMadeByAlastor31415926.helpers.STRING;
 import VampireMadeByAlastor31415926.powers.DelayedVulnerable;
-import basemod.abstracts.CustomCard;
 
-public class Indiscretion extends CustomCard{
+public class Indiscretion extends MyCard{
     private static final int VULNERABLENUMBER = 1;
     private static final int DRAWCARDSNUMBER = 3;
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(Indiscretion.class.getSimpleName());
     public static final String ID=STRING.makeID(Indiscretion.class.getSimpleName());
-    private static final String NAME=CARD_STRINGS.NAME;
-    private static final String IMG_PATH=STRING.makeIMG_PATH(Indiscretion.class.getSimpleName());
-    private static final int COST = 0;
-    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
-    private static final CardType TYPE = CardType.SKILL;
-    private static final CardColor COLOR = COLOR_BLOOD;
-    private static final CardRarity RARITY = CardRarity.RARE;
-    private static final CardTarget TARGET = CardTarget.SELF;
     public Indiscretion() {
-        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        super(Indiscretion.class.getSimpleName(),CARD_STRINGS,0,CardRarity.UNCOMMON, CardType.SKILL);
     }
     @Override
     public void upgrade() {
--- src/main/java/VampireMadeByAlastor31415926/cards/Strike.java
+++ src/main/java/VampireMadeByAlastor31415926/cards/Strike.java
@@ -10,29 +10,17 @@ import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
-import static VampireMadeByAlastor31415926.characters.MyCharacter.PlayerColorEnum.COLOR_BLOOD;
+import VampireMadeByAlastor31415926.helpers.MyCard;
 import VampireMadeByAlastor31415926.helpers.STRING;
-import basemod.abstracts.CustomCard;
 
-public class Strike extends CustomCard {
+public class Strike extends MyCard {
     private static final CardStrings CARD_STRINGS = CardCrawlGame.languagePack.getCardStrings(Strike.class.getSimpleName());
     public static final String ID=STRING.makeID(Strike.class.getSimpleName());
-    private static final String NAME=CARD_STRINGS.NAME;
-    private static final String IMG_PATH=STRING.makeIMG_PATH(Strike.class.getSimpleName());
-    private static final int COST = 1;
-    private static final String DESCRIPTION = CARD_STRINGS.DESCRIPTION;
-    private static final CardType TYPE = CardType.ATTACK;
-    private static final CardColor COLOR = COLOR_BLOOD;
-    private static final CardRarity RARITY = CardRarity.BASIC;
-    private static final CardTarget TARGET = CardTarget.ENEMY;
     public Strike() {
-        super(ID, NAME, IMG_PATH, COST, DESCRIPTION, TYPE, COLOR, RARITY, TARGET);
+        super(Strike.class.getSimpleName(),CARD_STRINGS,1,CardRarity.BASIC,CardType.ATTACK);
         this.damage = this.baseDamage = 6;
         this.tags.add(CardTags.STARTER_STRIKE);
         this.tags.add(CardTags.STRIKE);
-        // this.retain = true;
-        // this.isEthereal = true;
-        // this.isInnate = true;
     }
     @Override
     public void upgrade() {
